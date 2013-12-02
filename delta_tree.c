b125d9b799317a003d81c1611f9248b6c994812c
Brian Harring <ferringb@gmail.com>
2013-12-02 04:13:29 -0500

    Initial work adding a tool for working with directories.
    This involves both cfile changes (multifile backend, making multiple
    files appear as one contiguous space), and new binaries: delta_patcher and
    delta_tree .

---
+++ delta_tree.c
@@ -0,0 +1,192 @@
+/*
+  Copyright (C) 2003-2005 Brian Harring
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US 
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <cfile.h>
+#include <fcntl.h>
+#include "string-misc.h"
+#include <diffball/formats.h>
+#include <diffball/defs.h>
+#include <diffball/api.h>
+#include "options.h"
+#include <diffball/errors.h>
+
+unsigned int global_verbosity=0;
+
+char  *patch_format;
+
+struct option long_opts[] = {
+	STD_LONG_OPTIONS,
+	DIFF_LONG_OPTIONS,
+	FORMAT_LONG_OPTION("patch-format", 'f'),
+	END_LONG_OPTS
+};
+
+struct usage_options help_opts[] = {
+	STD_HELP_OPTIONS,
+	DIFF_HELP_OPTIONS,
+	FORMAT_HELP_OPTION("patch-format", 'f', "format to output the patch in"),
+	USAGE_FLUFF("differ expects 3 args- source, target, name for the patch\n"
+	"if output to stdout is enabled, only 2 args required- source, target\n"
+	"Example usage: differ older-version newerer-version upgrade-patch"),
+	END_HELP_OPTS
+};
+
+char short_opts[] = STD_SHORT_OPTIONS DIFF_SHORT_OPTIONS "f:";
+
+int main(int argc, char **argv)
+{
+	cfile out_cfh, ref_cfh, ver_cfh;
+	int out_fh;
+
+	int optr;
+	char  *src_file = NULL;
+	char  *trg_file = NULL;
+	char  *patch_name = NULL;
+	unsigned long patch_id = 0;
+	signed long encode_result=0;
+	int err;
+	unsigned long sample_rate=0;
+	unsigned long seed_len = 0;
+	unsigned long hash_size = 0;
+//	unsigned int patch_compressor = 0;
+	unsigned int patch_to_stdout = 0;
+
+	#define DUMP_USAGE(exit_code)		\
+		print_usage("differ", "src_file trg_file [patch_file|or to stdout]", help_opts, exit_code);
+
+	while((optr = getopt_long(argc, argv, short_opts, long_opts, NULL)) != -1) {
+		switch(optr) {
+		case OVERSION:
+			print_version("differ");		exit(0);
+		case OUSAGE:
+		case OHELP:
+			DUMP_USAGE(0);
+		case OVERBOSE:
+			global_verbosity++;				break;
+		case OSAMPLE:
+			sample_rate = atol(optarg);
+			if(sample_rate == 0 || sample_rate > MAX_SAMPLE_RATE) DUMP_USAGE(EXIT_USAGE);
+			break;
+		case OSEED:
+			seed_len = atol(optarg);
+			if(seed_len == 0 || seed_len > MAX_SEED_LEN) DUMP_USAGE(EXIT_USAGE);
+			break;
+		case OHASH:
+			hash_size = atol(optarg);
+			if(hash_size == 0 || hash_size > MAX_HASH_SIZE) DUMP_USAGE(EXIT_USAGE);
+			break;
+		case OSTDOUT:
+			patch_to_stdout = 1;		break;
+		case 'f':
+			patch_format = optarg;		break;
+		default:
+			v0printf("invalid arg- %s\n", argv[optind]);
+			DUMP_USAGE(EXIT_USAGE);
+		}
+	}
+	err = 0;
+	src_file = (char *)get_next_arg(argc, argv);
+	if (src_file) {
+		if (copen_multifile_directory(&ref_cfh, src_file)) {
+			v0printf("Walking directory %s failed\n", src_file);
+			exit(EXIT_USAGE);
+		}
+	}
+	exit(0);
+	
+	if( ((src_file = (char *)get_next_arg(argc,argv)) == NULL) ||
+		(err = copen(&ref_cfh, src_file, NO_COMPRESSOR, CFILE_RONLY)) != 0) {
+		if(src_file) {
+			if(err == MEM_ERROR) {
+				v0printf("alloc failure for src_file\n");
+			} else {
+				v0printf("Must specify an existing source file.\n");
+			}
+			exit(EXIT_USAGE);
+		}
+		DUMP_USAGE(EXIT_USAGE);
+	}
+	err = 0;
+	if( ((trg_file=(char *)get_next_arg(argc, argv)) == NULL) ||
+		(err = copen(&ver_cfh, trg_file, NO_COMPRESSOR, CFILE_RONLY)) != 0) {
+		if(trg_file) {
+			if(err == MEM_ERROR) {
+				v0printf("alloc failure for trg_file\n");
+			} else {
+				v0printf("Must specify an existing target file.\n");
+			}
+			exit(EXIT_USAGE);
+		}
+		DUMP_USAGE(EXIT_USAGE);
+	}
+	if(patch_to_stdout != 0) {
+		out_fh = 1;
+	} else {
+		if((patch_name = (char *)get_next_arg(argc, argv)) == NULL)
+			DUMP_USAGE(EXIT_USAGE);
+		if((out_fh = open(patch_name, O_WRONLY | O_TRUNC | O_CREAT, 0644))==-1) {
+			v0printf( "error creating patch file '%s' (open failed)\n", patch_name);
+			exit(1);
+		}
+	}
+	if (NULL!=get_next_arg(argc, argv)) {
+		DUMP_USAGE(EXIT_USAGE);
+	}
+
+	if(patch_format==NULL) {
+		patch_id = DEFAULT_PATCH_ID;
+	} else {
+		patch_id = check_for_format(patch_format, strlen(patch_format));
+		if(patch_id==0) {
+			v0printf( "Unknown format '%s'\n", patch_format);
+			exit(EXIT_FAILURE);
+		}
+	}
+	if(copen_dup_fd(&out_cfh, out_fh, 0, 0, NO_COMPRESSOR /* patch_compressor */, CFILE_WONLY)) {
+		v0printf("error allocing needed memory for output, exiting\n");
+		exit(EXIT_FAILURE);
+	}
+
+	v1printf("using patch format %lu\n", patch_id);
+	v1printf("using seed_len(%lu), sample_rate(%lu), hash_size(%lu)\n", 
+		seed_len, sample_rate, hash_size);
+	v1printf("verbosity level(%u)\n", global_verbosity);
+	v1printf("initializing Command Buffer...\n");
+
+	encode_result = simple_difference(&ref_cfh, &ver_cfh, &out_cfh, patch_id, seed_len, sample_rate, hash_size);
+	v1printf("flushing and closing out file\n");
+	cclose(&out_cfh);
+	close(out_fh);
+	if(err) {
+		if(! patch_to_stdout) {
+			unlink(patch_name);
+		}
+	}
+	v1printf("encode_result=%ld\n", encode_result);
+	v1printf("exiting\n");
+	v1printf("closing reference file\n");
+	cclose(&ref_cfh);
+	v1printf("closing version file\n");
+	cclose(&ver_cfh);
+	close(out_fh);
+	check_return2(encode_result, "encoding result was nonzero")
+	return 0;
+}
---
+++ libcfile/multifile.c
@@ -0,0 +1,370 @@
+/*
+  Copyright (C) 2003-2013 Brian Harring
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US 
+*/
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "internal.h"
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+#include <linux/limits.h>
+
+typedef struct {
+	size_t	start;
+	size_t	end;
+} file_position;
+
+typedef struct {
+	// Directory root for all files that were given.  Guranteed to have a trailig /
+	char *root;
+	int root_len;
+	// The sorted list of files.
+	char **files;
+	file_position *file_map;
+	unsigned long file_count;
+	int active_fd;
+	unsigned long current_file_index;
+	int flags;
+} multifile_data;
+
+void
+get_filepath(multifile_data *data, unsigned long file_pos, char *buf)
+{
+	if (data->root_len) {
+		memcpy(buf, data->root, data->root_len);
+		buf += data->root_len;
+	}
+	strcpy(buf, data->files[file_pos]);
+}
+
+int
+bsearch_compar(const void *key, const void *array_item)
+{
+	size_t offset = *((size_t *)key);
+	file_position *item = (file_position *)array_item;
+	if (offset < item->start) {
+		return -1;
+	} else if (offset < item->end) {
+		return 0;
+	}
+	return 1;
+}
+
+void
+multifile_close_active_fd(multifile_data *data)
+{
+	if (data->active_fd != -1) {
+		close(data->active_fd);
+		data->active_fd = -1;
+	}
+}
+
+int
+set_file_index(multifile_data *data, size_t offset)
+{
+	assert(offset <= data->file_map[data->file_count - 1].end);
+	file_position *match = (file_position *)bsearch(&offset, data->file_map, data->file_count, sizeof(file_position), bsearch_compar);
+	if (!match) {
+		eprintf("Somehow received NULL from bsearch for multitfile: offset %li\n", offset);
+		return 1;
+	}
+	multifile_close_active_fd(data);
+	data->current_file_index = match - data->file_map;
+	assert (data->current_file_index < data->file_count);
+	return 0;
+}
+
+unsigned int
+cclose_multifile(cfile *cfh, void *raw)
+{
+	multifile_data *data = (multifile_data *)raw;
+	if (data) {
+		multifile_close_active_fd(data);
+		free(data->file_map);
+		free(data->files);
+		free(data->root);
+		free(data);
+	}
+	return 0;
+}
+
+ssize_t
+cseek_multifile(cfile *cfh, void *raw, ssize_t orig_offset, ssize_t data_offset, int offset_type)
+{
+	dcprintf("requested to cseek to %lu\n", data_offset);
+	multifile_data *data = (multifile_data *)raw;
+	cfh->data.pos = 0;
+	cfh->data.end = 0;
+	if (data_offset >= data->file_map[data->current_file_index].end || data_offset < data->file_map[data->current_file_index].start) {
+		// This requires a new file; jump to that file.
+		if (set_file_index(data, data_offset)) {
+			return (cfh->err = IO_ERROR);
+		}
+	}
+	if(multifile_ensure_open_active(data)) {
+		return (cfh->err = IO_ERROR);
+	}
+	cfh->data.offset = data_offset;
+	size_t desired = data_offset - data->file_map[data->current_file_index].start;
+	if (desired != lseek(data->active_fd, desired, SEEK_SET)) {
+		eprintf("Somehow failed to lseek to %lu for %s\n", desired, data->files[data->current_file_index]);
+		return (cfh->err = IO_ERROR);
+	}
+	// Check this; CSEEK_ABS behaviour may be retarded.
+	return data_offset;
+}
+
+int
+multifile_ensure_open_active(multifile_data *data)
+{
+	char buf[PATH_MAX];
+	if (data->active_fd == -1) {
+		get_filepath(data, data->current_file_index, buf);
+		data->active_fd = open(buf, O_NOFOLLOW|O_RDONLY);
+		if (data->active_fd == -1) {
+			eprintf("Failed opening multifile %s; errno %i\n", buf, errno);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int
+crefill_multifile(cfile *cfh, void *raw)
+{
+	multifile_data *data = (multifile_data *)raw;
+	cfh->data.offset += cfh->data.end;
+	cfh->data.end = cfh->data.pos = 0;
+
+	unsigned long current = data->current_file_index;
+
+	// Seek forward for the next file.
+	// This should be a single step, but zero length files may be in
+	// the list- as such we use a while loop here to skip them.
+	while (cfh->data.offset >= data->file_map[current].end) {
+		if (current + 1 == data->file_count) {
+			cfh->state_flags |= CFILE_EOF;
+			cfh->data.offset += cfh->data.end;
+			cfh->data.pos = cfh->data.end = 0;
+			return 0;
+		}
+		current++;
+	}
+	if (current != data->current_file_index) {
+		data->current_file_index = current;
+		multifile_close_active_fd(data);
+	}
+	if (multifile_ensure_open_active(data)) {
+		return (cfh->err = IO_ERROR);
+	}
+	size_t desired = cfh->data.offset - data->file_map[data->current_file_index].start;
+	if (desired != lseek(data->active_fd, desired, SEEK_SET)) {
+		eprintf("Somehow lseek w/in refill failed\n");
+		return (cfh->err = IO_ERROR);
+	}
+	cfh->data.end = read(data->active_fd, cfh->data.buff,
+		MIN(cfh->data.size, data->file_map[data->current_file_index].end - cfh->data.offset));
+	dcprintf("crefill_multifile: %u: no_compress, got %lu\n", cfh->cfh_id, cfh->data.end);
+	return 0;
+}
+
+int
+copen_multifile(cfile *cfh, char *root, char *files[], unsigned long file_count)
+{
+	int result = 0;
+	memset(cfh, 0, sizeof(cfile));
+	multifile_data *data = (multifile_data *)calloc(sizeof(multifile_data), 1);
+	if (!data) {
+		result = MEM_ERROR;
+		goto cleanup;
+	}
+	data->file_map = calloc(sizeof(file_position), file_count);
+	if (!data->file_map) {
+		result = MEM_ERROR;
+		goto cleanup;
+	}
+	data->root = root;
+	data->root_len = strlen(root);
+	data->files = files;
+	data->file_count = file_count;
+	data->active_fd = -1;
+
+	char buf[PATH_MAX];
+	struct stat st;
+	size_t position = 0;
+	unsigned long x = 0;
+	char *file = files[0];
+	strcpy(buf, root);
+	for (; x < file_count; x++, file++) {
+		get_filepath(data, x, buf);
+		result = lstat(buf, &st);
+		if (result != 0) {
+			result = IO_ERROR;
+			eprintf("Failed initialized multifile due to errno %i for %s\n", errno, buf);
+			goto cleanup;
+		}
+		data->file_map[x].start = position;
+		position += st.st_size;
+		data->file_map[x].end = position;
+	}
+
+	result = internal_copen(cfh, -1, 0, position, 0, 0, NO_COMPRESSOR, CFILE_RONLY);
+	if (result) {
+		goto cleanup;
+	}
+
+	cfh->io.seek = cseek_multifile;
+	cfh->io.refill = crefill_multifile;
+	cfh->io.close = cclose_multifile;
+	cfh->io.data = (void *)data;
+	
+	return 0;
+
+cleanup:
+	if (data) {
+		cclose_multifile(cfh, data);
+	} else {
+		free(root);
+		free(files);
+	}
+	return result;
+}	
+
+int
+multifile_recurse_directory(const char *root, const char *directory, char **files[], unsigned long *files_count, unsigned long *files_size)
+{
+	DIR *the_dir;
+	struct dirent *entry;
+	char buf[PATH_MAX];
+	strcpy(buf, root);
+	char *directory_start = buf + strlen(root);
+	char *start = directory_start;
+	if (directory) {
+		strcpy(directory_start, directory);
+		start += strlen(directory);
+	}
+
+	if (!(the_dir = opendir(buf))) {
+		eprintf("multifile: Failed opening directory %s, errno %i\n", directory, errno);
+		return 1;
+	}
+
+	while ((entry = readdir(the_dir))) {
+		const char *name = entry->d_name;
+		if (name[0] == '.' && (name[1] == 0 || (name[1] == '.' && name[2] == 0))) {
+			// . or .. ; ignore.
+			continue;
+		}
+		start[0] = 0;
+		if (entry->d_type == DT_UNKNOWN) {
+			strcpy(start, name);
+			struct stat st;
+			if (lstat(buf, &st)) {
+				eprintf("multifile: Failed lstating %s; errno %i\n", buf, errno);
+				return 1;
+			}
+
+			if (!S_ISREG(st.st_mode)) {
+				if (S_ISDIR(st.st_mode)){
+					strcat(start, "/");;
+					if(multifile_recurse_directory(root, directory_start, files, files_count, files_size)) {
+						return 1;
+					}
+				}
+				continue;
+			}
+		} else if (entry->d_type != DT_REG) {
+			if (entry->d_type == DT_DIR) {
+				strcat(start, name);
+				strcat(start, "/");
+				if (multifile_recurse_directory(root, directory_start, files, files_count, files_size)) {
+					return 1;
+				}
+			}
+			continue;
+		} else {
+			strcpy(start, name);
+		}
+			
+		if (*files_size == *files_count) {
+			if (*files_size == 0) {
+				*files_size = 16;
+			}
+			char **tmp = realloc(*files, sizeof(char *) * ((*files_size) * 2));
+			if (!tmp) {
+				eprintf("multifile: failed reallocing files array to size %lu\n", ((*files_size) * 2));
+				return 1;
+			}
+			*files = tmp;
+			*files_size *= 2;
+		}
+		(*files)[*files_count] = strdup(directory_start);
+		if (!((*files)[*files_count])) {
+			eprintf("multifile: failed strdup for %s\n", buf);
+			return 1;
+		}
+		*files_count += 1;
+	}
+	closedir(the_dir);
+	return 0;
+}		
+
+static int
+cmpstrcmp(const void *p1, const void *p2)
+{
+	return strcmp(* (char * const *) p1, * (char * const *) p2);
+}
+
+int
+copen_multifile_directory(cfile *cfh, const char *src_directory)
+{
+	char **files = NULL;
+	unsigned long files_count = 0;
+	unsigned long files_size = 0;
+	char *directory = NULL;
+
+	int dir_len = strlen(src_directory);
+	if (src_directory[dir_len] != '/') {
+		directory = malloc(dir_len + 2);
+		if(directory) {
+			memcpy(directory, src_directory, dir_len);
+			directory[dir_len] = '/';
+			dir_len++;
+			directory[dir_len] = 0;
+		}
+	} else {
+		directory = strdup(src_directory);
+	}
+	if (!directory) {
+		eprintf("multifile: directory dup mem allocaiton failed\n");
+		return 1;
+	}
+	if (multifile_recurse_directory(directory, NULL, &files, &files_count, &files_size)) {
+		free(directory);
+		return 1;
+	}
+	qsort(files, files_count, sizeof(char *), cmpstrcmp);
+/*
+	unsigned long i=0;
+	for (; i < files_count; i++) {
+		printf("%s\n", files[i]);
+	}
+*/
+	return copen_multifile(cfh, directory, files, files_count);
+}
