fe825b2a2f58721af56a92b9645a7cdb8f181f8d
Brian Harring <ferringb@gmail.com>
2013-12-05 15:46:07 -0500

    Enforce explicit seek relative to the file start; without this, it assumes it starts reading from 0th byte, which is broken

--- libdiffball/gdiff.c
+++ libdiffball/gdiff.c
@@ -207,7 +207,7 @@ gdiffReconstructDCBuff(DCB_SRC_ID  src_id, cfile *patchf, CommandBuffer *dcbuff,
 	else
 		off_is_sbytes=0;
 //	assert(DCBUFFER_FULL_TYPE == dcbuff->DCBtype);
-	cseek(patchf, 5, CSEEK_CUR);
+	cseek(patchf, 5, CSEEK_FSTART);
 
 	add_id = DCB_REGISTER_VOLATILE_ADD_SRC(dcbuff, patchf, NULL, 0);
 //	ref_id = DCB_REGISTER_COPY_SRC(dcbuff, ref_cfh, NULL, 0);
