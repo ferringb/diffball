3b352570c51c66a5c94856f1bc0af33a4a3e313e
Brian Harring <ferringb@gmail.com>
2013-12-18 16:34:33 -0500

    Remove aclocal/automake 1.11 dep

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 touch ChangeLog
-aclocal-1.11 || { echo "failed aclocal"; exit 1; };
+aclocal || { echo "failed aclocal"; exit 1; };
 libtoolize --automake -c -f || { echo "failed libtoolize"; exit 1; }
 autoconf || { echo "failed autoconf"; exit 1; }
 autoheader || { echo "failed autoheader"; exit ; }
-automake-1.11 -a -c || { "echo failed automake"; exit 1; }
+automake -a -c || { "echo failed automake"; exit 1; }
 
 if [ -x ./test.sh ] ; then
 	exec ./test.sh "$@"
